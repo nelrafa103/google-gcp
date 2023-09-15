4a8e80e7e9b0f0806f23190f6aa91bb7078381e3
nelrafa103 <nelcidodiaz13@gmail.com>
2023-09-15 09:37:21 -0400

    Añadiendo docker a express

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const path = require("path")
 // Habilitando modulo para obtener valores en json (wow)
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+    //res.send('Hello World!')
 })
 
 app.listen(port, () => {
@@ -19,3 +22,5 @@ app.post("/",(req, res) => {
         respuesta: "Valida"
     })
 })
+
+
